20eef83a1157dd2bc3204ac4ed3c1f1186076078
ipiranhaa <tanatezp@gmail.com>
2016-04-21 23:52:00 +0700

    EQ UI

--- asset/js/script.js
+++ asset/js/script.js
@@ -76,10 +76,55 @@ $( document ).ready(function() {
 		}
 	});
 	
-	$(".knob").knob({
-		width: '100',
-		thickness: ".2",
-		bgColor: "#eee",
+	$(".eq-in").knob({
+		'width': 100,
+		'thickness': 0.2,
+		'bgColor': "#eee",
+		'min': -50,
+		'max': 10,
+		'step': 0.5,
+		'skin': 'tron',
+		'angleOffset': -125,
+		'angleArc': 250,
+		draw : function () {
+      // "tron" case
+      if (this.$.data('skin') == 'tron') {
+      	this.cursorExt = 0.3;
+					var a = this.arc(this.cv)  // Arc
+						, pa                   // Previous arc
+						, r = 1;
+						this.g.lineWidth = this.lineWidth;
+						if (this.o.displayPrevious) {
+							pa = this.arc(this.v);
+							this.g.beginPath();
+							this.g.strokeStyle = this.pColor;
+							this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, pa.s, pa.e, pa.d);
+							this.g.stroke();
+						}
+						this.g.beginPath();
+						this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+						this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, a.s, a.e, a.d);
+						this.g.stroke();
+						this.g.lineWidth = 2;
+						this.g.beginPath();
+						this.g.strokeStyle = this.o.fgColor;
+						this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+						this.g.stroke();
+						return false;
+					}
+				}
+			});
+			
+	$(".hp-filter").knob({
+		'width': 100,
+		'thickness': 0.2,
+		'bgColor': "#eee",
+		'min': 20,
+		'max': 250,
+		'step': 0.5,
+		'skin': 'tron',
+		'angleOffset': -125,
+		'angleArc': 999,
 		draw : function () {
       // "tron" case
       if (this.$.data('skin') == 'tron') {
