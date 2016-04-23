d144d4623cf58f97ce3a5ea729e32c73ccdee85e
Tanate <tanate.me@throughwave.co.th>
2016-04-23 20:24:25 +0700

    add volume control UI

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,19 +6,16 @@ $( document ).ready(function() {
 		value: 0,
 		orientation: 'vertical',
 		reversed: true
-		// ticks: [-1, -2, -3, -4, -5, -6, -7, -8, -9, 0, 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10],
-    // ticks_labels: ['-1', '-2', '-3', '-4', '-5', '-6', '-7', '-8', '-9', '0', '1', '2', '3', '4', '5', '6', '7', '8' ,'9', '10'],
-    // ticks_snap_bounds: 30
-  }
+	}
 
-  for (i = 0; i < 60; i++) {
-  	var ele = '#eq' + i.toString();
-  	$(ele).slider(options);
+	for (i = 0; i < 60; i++) {
+		var ele = '#eq' + i.toString();
+		$(ele).slider(options);
 
-  	if (i < 29 || (i > 29 && i < 59)) {
-  		$(ele).before(scaleElement);
-  	}
-  }
+		if (i < 29 || (i > 29 && i < 59)) {
+			$(ele).before(scaleElement);
+		}
+	}
 
 	// blind slider event
 	var filterTopData, filterBottomData;
@@ -82,75 +79,45 @@ $( document ).ready(function() {
 		'bgColor': "#eee",
 		'min': -50,
 		'max': 10,
-		'step': 0.5,
+		'step': 1,
 		'skin': 'tron',
 		'angleOffset': -125,
 		'angleArc': 250,
-		draw : function () {
-      // "tron" case
-      if (this.$.data('skin') == 'tron') {
-      	this.cursorExt = 0.3;
-					var a = this.arc(this.cv)  // Arc
-						, pa                   // Previous arc
-						, r = 1;
-						this.g.lineWidth = this.lineWidth;
-						if (this.o.displayPrevious) {
-							pa = this.arc(this.v);
-							this.g.beginPath();
-							this.g.strokeStyle = this.pColor;
-							this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, pa.s, pa.e, pa.d);
-							this.g.stroke();
-						}
-						this.g.beginPath();
-						this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
-						this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, a.s, a.e, a.d);
-						this.g.stroke();
-						this.g.lineWidth = 2;
-						this.g.beginPath();
-						this.g.strokeStyle = this.o.fgColor;
-						this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
-						this.g.stroke();
-						return false;
-					}
-				}
-			});
-			
+		'width': 60,
+		'change' : function (v) { console.log(v); }
+	});
+
 	$(".hp-filter").knob({
 		'width': 100,
 		'thickness': 0.2,
 		'bgColor': "#eee",
 		'min': 20,
 		'max': 250,
-		'step': 0.5,
+		'step': 1,
 		'skin': 'tron',
 		'angleOffset': -125,
-		'angleArc': 999,
-		draw : function () {
-      // "tron" case
-      if (this.$.data('skin') == 'tron') {
-      	this.cursorExt = 0.3;
-					var a = this.arc(this.cv)  // Arc
-						, pa                   // Previous arc
-						, r = 1;
-						this.g.lineWidth = this.lineWidth;
-						if (this.o.displayPrevious) {
-							pa = this.arc(this.v);
-							this.g.beginPath();
-							this.g.strokeStyle = this.pColor;
-							this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, pa.s, pa.e, pa.d);
-							this.g.stroke();
-						}
-						this.g.beginPath();
-						this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
-						this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, a.s, a.e, a.d);
-						this.g.stroke();
-						this.g.lineWidth = 2;
-						this.g.beginPath();
-						this.g.strokeStyle = this.o.fgColor;
-						this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
-						this.g.stroke();
-						return false;
-					}
-				}
-			});
+		'angleArc': 250,
+		'change' : function (v) { console.log(v); }
+	});
+
+	$('.hp-filter-btn').click(function(e)	{
+		console.log('hp switch clicked');
+		e.preventDefault();
+		if ($(this).hasClass('pressed')) {
+			$(this).removeClass('pressed');
+		} else {
+			$(this).addClass('pressed');
+		}
+	});
+
+	$('.eq-in-btn').click(function(e)	{
+		console.log('eq switch clicked');
+		e.preventDefault();
+		if ($(this).hasClass('pressed')) {
+			$(this).removeClass('pressed');
+		} else {
+			$(this).addClass('pressed');
+		}
+	});
+
 });
